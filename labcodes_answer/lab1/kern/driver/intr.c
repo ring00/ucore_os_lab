601c62a91cd2a2488eb498a97a25ad224b1953d0
Wei Zhang <zhangwei15@mails.tsinghua.edu.cn>
2018-12-11 15:29:55 +0800

    Clean up code in lab1

--- labcodes_answer/lab1/kern/driver/intr.c
+++ labcodes_answer/lab1/kern/driver/intr.c
@@ -3,12 +3,10 @@
 
 /* intr_enable - enable irq interrupt */
 void intr_enable(void) {
-    // set_csr(sstatus, SSTATUS_SIE);
-    asm volatile ("csrrsi x0, sstatus, %0": : "i"(SSTATUS_SIE));
+    set_csr(sstatus, SSTATUS_SIE);
 }
 
 /* intr_disable - disable irq interrupt */
 void intr_disable(void) {
-    // clear_csr(sstatus, SSTATUS_SIE);
-    asm volatile ("csrrci x0, sstatus, %0": : "i"(SSTATUS_SIE));
+    clear_csr(sstatus, SSTATUS_SIE);
 }
